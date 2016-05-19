d5fbf9af00417eaa5de6f6800d568aed9cc39a25
mcizo <maciz@outlook.fr>
2016-05-19 21:56:50 +0200

    feat: affichage des infos sur la machine

--- main.hs
+++ main.hs
@@ -5,9 +5,47 @@ import Data.ByteString.Lazy as B(readFile)
 import System.Environment
 import Data.Aeson as Aeson
 import qualified Data.Sequence as Seq
+import qualified Data.Map.Strict as Map
 
-genInputSeq :: String -> Seq.Seq String
-genInputSeq = undefined
+
+goodElement :: [String] -> String -> Maybe (String, Int)
+goodElement [] _ = Nothing
+goodElement (x:xs) input
+    | x == take len input = Just (x, len)
+    | otherwise           = goodElement xs input
+    where
+        len = length x
+
+genInputSeq :: String -> [String] -> Seq.Seq String -> Seq.Seq String
+genInputSeq "" _ acc          = acc
+genInputSeq input symbols acc = case goodElement symbols input of
+    Just (symbol, len) -> genInputSeq (drop len input) symbols (acc Seq.|> symbol)
+    Nothing            -> Seq.empty
+
+printTransitions :: Map.Map String [Transition] -> IO ()
+printTransitions ts
+    | Map.size ts == 0 = return ()
+    | otherwise        = do
+        let tmp = Map.elemAt 0 ts
+        putStr $ fst tmp ++ ": "
+        print $ snd tmp
+        printTransitions $ Map.delete (fst tmp) ts
+
+showMachine :: Machine -> IO ()
+showMachine x = do
+    putStrLn (name x)
+    putStrLn "********\n"
+    putStr "Aphabet: "
+    print $ alphabet x
+    putStrLn ("Blank: " ++ blank x)
+    putStr "States: "
+    print $ states x
+    putStrLn $ "Initial: " ++ initial x
+    putStr "Finals: "
+    print $ finals x
+    putStrLn ""
+    printTransitions (transitions x)
+    return ()
 
 main :: IO ()
 main = do
@@ -17,9 +55,9 @@ main = do
         putStrLn "Error. Parameters!!"
         return ()
     else do
-        myJson <- B.readFile $ args !! 0
+        myJson <- B.readFile $ head args
         let parsed = Aeson.eitherDecode myJson :: Either String Machine
         case parsed of
-            Right x -> return ()
-            otherwise -> return ()
+            Right x -> showMachine x
+            _       -> return ()
         return ()
